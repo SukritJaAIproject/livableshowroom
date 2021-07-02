272589d02b404997fe8e29cc3e40338c3c766c8e
Sukrit Jaidee <45908411+SukritJaAIproject@users.noreply.github.com>
2021-07-02 11:18:03 +0700

    upload

--- main/control_device/getdata_from_airconet.py
+++ main/control_device/getdata_from_airconet.py
@@ -18,3 +18,19 @@ envTempShow = x2[0]['deviceStatus']['deviceStatus']['envTempShow']
 tgtTemp = x2[0]['deviceStatus']['deviceStatus']['tgtTemp']
 onoff = x2[0]['deviceStatus']['deviceStatus']['onoff']
 print(mode, fan, envTemp, envTempShow, tgtTemp, onoff)
+
+
+'''
+import requests
+import json
+
+url = "https://www.activate-ac.com/dev/deviceStatusSetPointCurrentStatus"
+payload = {}
+headers = {
+    'Cookie': 'JSESSIONID=node0rn5sn3feu38d5rbgx853kmgc3181.node0'
+}
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+x1 = response.text
+x2 = json.loads(x1)
+print('x2', x2)
+'''
